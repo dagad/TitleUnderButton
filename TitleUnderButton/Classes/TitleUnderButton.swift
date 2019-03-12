d85968dc83338681fca2a44493869f9f63d644ea
Sangyeol Kang <dagad906@gmail.com>
2019-03-13 07:45:42 +0900

    add TitleUnderButton class

---
+++ TitleUnderButton/Classes/TitleUnderButton.swift
@@ -0,0 +1,20 @@
+//
+//  TitleUnderButton.swift
+//  Pods
+//
+//  Created by Sangyeol Kang on 13/03/2019.
+//
+
+import UIKit
+
+class TitleUnderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
